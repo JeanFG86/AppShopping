2bfe510f8d0c28f901d355f5905ddee033774772
jeanfg86 <jeanfg86@gmail.com>
2022-05-24 21:35:29 -0300

    add SearchCommand

---
+++ AppShopping/AppShopping/ViewModels/StoresViewModel.cs
@@ -0,0 +1,32 @@
+﻿using AppShopping.Libraries.Enums;
+using AppShopping.Models;
+using AppShopping.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppShopping.ViewModels
+{
+    public class StoresViewModel
+    {
+        public string SearchWord { get; set; }
+        public ICommand SearchCommand { get; set; }
+        public List<Establishment> Establishments { get; set; }
+
+        public StoresViewModel()
+        {
+            SearchCommand = new Command(Search);
+
+            var allEstablishment = new EstablishmentService().GetEstablishment();
+            var allStores = allEstablishment.Where(x => x.Type == EstablishmentType.Store).ToList();
+
+            Establishments = allStores;
+        }
+
+        private void Search()
+        {
+
+        }
+    }
+}
