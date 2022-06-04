b6e4bc5ac7cf798ba8c84943b93c7b5da8341f89
jeanfg86 <jeanfg86@gmail.com>
2022-06-04 17:24:52 -0300

    create abstacts class StoresViewModel

---
+++ AppShopping/AppShopping/ViewModels/EstablishmentViewModel.cs
@@ -0,0 +1,59 @@
+﻿using AppShopping.Libraries.Enums;
+using AppShopping.Libraries.Helpers.mvvm;
+using AppShopping.Models;
+using AppShopping.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppShopping.ViewModels
+{
+    public abstract class EstablishmentViewModel : BaseViewModel
+    {
+        public string SearchWord { get; set; }
+        public ICommand SearchCommand { get; set; }
+
+        private List<Establishment> _establishments;
+
+        public List<Establishment> Establishments
+        {
+            get { return _establishments; }
+            set { SetProperty(ref _establishments, value); }
+        }
+
+
+        //public List<Establishment> Establishments { get; set; }
+        public List<Establishment> _allEstablishment { get; set; }
+
+        public ICommand DetailCommand { get; set; }
+
+        private readonly EstablishmentType _establishmentType;
+
+        public EstablishmentViewModel(EstablishmentType establishmentType)
+        {
+            _establishmentType = establishmentType;
+            SearchCommand = new Command(Search);
+            DetailCommand = new Command<Establishment>(Detail);
+
+            var allEstablishment = new EstablishmentService().GetEstablishment();
+            var allStores = allEstablishment.Where(x => x.Type == _establishmentType).ToList();
+
+            Establishments = allStores;
+            _allEstablishment = allStores;
+        }
+
+        private void Detail(Establishment establishment)
+        {
+            String establishmentSerialized = JsonConvert.SerializeObject(establishment);
+            Shell.Current.GoToAsync($"establishment/detail?establishmentSerialized={Uri.EscapeDataString(establishmentSerialized)}");
+        }
+
+        private void Search()
+        {
+            Establishments = _allEstablishment.Where(x => x.Name.ToLower().Contains(SearchWord.ToLower())).ToList();
+        }
+    }
+}
--- AppShopping/AppShopping/ViewModels/StoresViewModel.cs
+++ AppShopping/AppShopping/ViewModels/StoresViewModel.cs
@@ -11,46 +11,10 @@ using Xamarin.Forms;
 
 namespace AppShopping.ViewModels
 {
-    public class StoresViewModel : BaseViewModel
+    public class StoresViewModel : EstablishmentViewModel
     {
-        public string SearchWord { get; set; }
-        public ICommand SearchCommand { get; set; }
-
-        private List<Establishment> _establishments;
-
-        public List<Establishment> Establishments
+        public StoresViewModel(EstablishmentType establishmentType) : base(establishmentType)
         {
-            get { return _establishments; }
-            set { SetProperty(ref _establishments, value); }
-        }
-
-
-        //public List<Establishment> Establishments { get; set; }
-        public List<Establishment> _allEstablishment { get; set; }
-
-        public ICommand DetailCommand { get; set; }
-
-        public StoresViewModel()
-        {
-            SearchCommand = new Command(Search);
-            DetailCommand = new Command<Establishment>(Detail);
-
-            var allEstablishment = new EstablishmentService().GetEstablishment();
-            var allStores = allEstablishment.Where(x => x.Type == EstablishmentType.Store).ToList();
-
-            Establishments = allStores;
-            _allEstablishment = allStores;
-        }
-
-        private void Detail(Establishment establishment)
-        {
-            String establishmentSerialized = JsonConvert.SerializeObject(establishment);
-            Shell.Current.GoToAsync($"establishment/detail?establishmentSerialized={Uri.EscapeDataString(establishmentSerialized)}");
-        }
-
-        private void Search()
-        {
-            Establishments = _allEstablishment.Where(x => x.Name.ToLower().Contains(SearchWord.ToLower())).ToList();
         }
     }
 }
